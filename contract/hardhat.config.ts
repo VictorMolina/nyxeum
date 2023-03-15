832a5c411521541b3be60ce7f1828059d4a6ffc8
Victor Molina <victor.molina@scopely.com>
2023-03-15 23:49:26 +0100

    Use chainId 1337 for development

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -4,6 +4,15 @@ import "@openzeppelin/hardhat-upgrades";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  defaultNetwork: "localhost",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    localhost: {
+      chainId: 1337
+    }
+  }
 };
 
 export default config;
