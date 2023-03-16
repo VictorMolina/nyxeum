4586e2d97f543e392e5244b28602f098814d8125
Victor Molina <victor.molina@scopely.com>
2023-03-16 23:14:43 +0100

    Configure external images on Next.js

--- dapp/next.config.js
+++ dapp/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'nyxeum.vercel.app',
+        port: '',
+        pathname: '/nft/**'
+      }
+    ],
+    formats: ['image/avif', 'image/webp']
+  }
 }
 
 module.exports = nextConfig
