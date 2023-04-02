42628a0018bb24bbe6f8fdea922e20da76f1530f
Victor Molina <victor.molina@scopely.com>
2023-04-02 17:42:33 +0200

    Set NYX buy price

--- contract/contracts/NyxEssence.sol
+++ contract/contracts/NyxEssence.sol
@@ -8,9 +8,16 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 // Conversion 1_000 NYX = 1 ETH
 contract NyxEssence is ERC20, Ownable {
 
+    uint256 private _buyPrice;
+
     constructor() ERC20("Nyx Essence", "NYX") {
         uint initial_supply = 1_000_000 * (10 ** decimals());
         mint(address(this), initial_supply);
+        _buyPrice = 1_000;
+    }
+
+    function setBuyPrice(uint256 buyPrice) public onlyOwner {
+        _buyPrice = buyPrice;
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
@@ -19,8 +26,8 @@ contract NyxEssence is ERC20, Ownable {
 
     function buy() payable public {
         require(msg.value > 0, "You need to pay some ETH");
-        require(msg.value * 1_000 <= balanceOf(address(this)), "Not enough NYX in the reserve");
+        require(msg.value * _buyPrice <= balanceOf(address(this)), "Not enough NYX in the reserve");
 
-        _transfer(address(this), msg.sender, msg.value * 1_000);
+        _transfer(address(this), msg.sender, msg.value * _buyPrice);
     }
 }
