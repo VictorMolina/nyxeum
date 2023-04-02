8ce03ecc913136779a22474d9038992678ddecf4
Victor Molina <victor.molina@scopely.com>
2023-04-02 17:04:13 +0200

    Handle error message on attack commit

--- dapp/components/game/hero-attack/HeroAttackCommit.tsx
+++ dapp/components/game/hero-attack/HeroAttackCommit.tsx
@@ -1,7 +1,7 @@
 import styles from "@/styles/Home.module.css";
 import {useAttackCommit} from "@/components/utils/NyxeumGameProxyHooks";
-import {BigNumber, utils} from "ethers";
-import {FormEvent, useEffect, useState} from "react";
+import {BigNumber} from "ethers";
+import {FormEvent, useState} from "react";
 import useDebounce from "@/components/utils/Debounce";
 
 const HeroAttackCommit = ({ tokenId, setMessage }: Props) => {
@@ -11,28 +11,25 @@ const HeroAttackCommit = ({ tokenId, setMessage }: Props) => {
 
     const heroAttackCommit = useAttackCommit(tokenId, debouncedTargetId);
 
-    useEffect(() => {
-        if (targetId && debouncedTargetId && heroAttackCommit && heroAttackCommit.write === undefined) {
-            setTargetId(undefined);
-            const targetName = `Hero Of Nyxeum #${debouncedTargetId}`;
-            const popup = (
-                <div className={styles.popupMessage}>
-                    <p style={{ fontWeight: "bold"}}>Attack Log</p>
-                    <br />
-                    <p>ERROR: Is not allowed to attack to {targetName}</p>
-                    <br />
-                    <button className={styles.button} onClick={() => {
-                        setMessage(undefined);
-                    }}>OK</button>
-                </div>
-            );
-            setMessage(popup);
-        }
-    }, [heroAttackCommit, setMessage, targetId, debouncedTargetId]);
+    const displayError = () => {
+        const targetName = `Hero Of Nyxeum #${debouncedTargetId}`;
+        const popup = (
+            <div className={styles.popupMessage}>
+                <p style={{ fontWeight: "bold"}}>Attack Log</p>
+                <br />
+                <p>ERROR: Is not allowed to attack to {targetName}</p>
+                <br />
+                <button className={styles.button} onClick={() => {
+                    setMessage(undefined);
+                }}>OK</button>
+            </div>
+        );
+        setMessage(popup);
+    };
 
     return (
         <>
-            <button className={styles.button} disabled={!targetId} onClick={() => heroAttackCommit?.write?.()}>Attack for 1.0 NYX</button>
+            <button className={styles.button} disabled={!targetId} onClick={() => heroAttackCommit?.write ? heroAttackCommit?.write?.() : displayError() }>Attack for 1.0 NYX</button>
             <input className={styles.button} type="number" id="opponent" name="opponent" required={true} min={1} max={9999} placeholder="Opponent ID" onChange={(e: FormEvent<HTMLInputElement>) => setTargetId(e?.currentTarget?.value ? BigNumber.from(e.currentTarget.value) : undefined)}></input>
         </>
     );
