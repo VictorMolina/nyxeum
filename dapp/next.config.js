3cb1a3841806e211f22a69771e2824278b9e9e37
Victor Molina <victor.molina@scopely.com>
2023-04-10 20:55:57 +0200

    Allow CORS from all origins for /api/nft

--- dapp/next.config.js
+++ dapp/next.config.js
@@ -16,6 +16,19 @@ const nextConfig = {
     NYX_ESSENCE_CONTRACT_ADDRESS: process.env.NYX_ESSENCE_CONTRACT_ADDRESS,
     HEROES_OF_NYXEUM_CONTRACT_ADDRESS: process.env.HEROES_OF_NYXEUM_CONTRACT_ADDRESS,
     NYXEUM_GAME_PROXY_ADDRESS: process.env.NYXEUM_GAME_PROXY_ADDRESS
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/nft/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+        ]
+      }
+    ]
   }
 }
 
